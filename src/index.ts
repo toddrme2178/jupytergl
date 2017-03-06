bab509e62245dd130435bd6598963dea850a83da
Vidar Tonaas Fauske <vidartf@gmail.com>
2017-03-06 15:01:47 +0100

    Functions for processing messages

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 
+import {
+  JSONArray, JSONValue, JSONPrimitive
+} from './json';
 
 export
 function availableMethods(gl: WebGLRenderingContext): string[] {
@@ -25,4 +28,49 @@ function availableConstants(gl: WebGLRenderingContext): string[] {
     }
   }
   return ret;
+}
+
+
+export
+interface IInstruction {
+  type: 'exec' | 'query';
+  op: string;
+  args: JSONArray;
+}
+
+
+export
+function execMessage(gl: WebGLRenderingContext, message: IInstruction[]): void {
+  for (let instruction of message) {
+    execInstruction(gl, instruction);
+  }
+}
+
+
+export
+function queryMessage(gl: WebGLRenderingContext, message: IInstruction[]): void {
+  for (let i = 0; i < message.length - 1; ++i) {
+    execInstruction(gl, message[i]);
+  }
+  return queryInstruction(gl, message[message.length - 1]);
+}
+
+
+/**
+ * Execute an instruction, discarding any return value.
+ *
+ * Throws an error if instruction is missing.
+ */
+function execInstruction(gl: WebGLRenderingContext, instruction: IInstruction): void {
+  (gl as any)[instruction.op](...instruction.args);
+}
+
+
+/**
+ * Process an instruction, returning its return value.
+ *
+ * Throws an error if instruction is missing.
+ */
+function queryInstruction(gl: WebGLRenderingContext, instruction: IInstruction): void {
+  return (gl as any)[instruction.op](...instruction.args);
 }
